40f2fc242947f2e0c8e1c0a53f2ff9a49217b8bd
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2023-02-08 23:35:39 +0600

    3rd commit

--- 4main.js
+++ 4main.js
@@ -1,18 +1,3 @@
-// #002Write a JavaScript program to round a number to a specified amount of digits.
-
-//#Source https://bit.ly/2neWfJ2 
-const round = (n, decimals = 0) => Number(`${Math.round(`${n}e${decimals}`)}e-${decimals}`);
-
-console.log(round(1.005, 2));
-console.log(round(1.05, 2));
-console.log(round(1.0005, 2));
-
-
-//#Source https://bit.ly/2neWfJ2 
-const reverseString = str => [...str].reverse().join('');
-
-console.log(reverseString('php')); 
-console.log(reverseString('foobar'));
 // Write a JavaScript program that takes a predicate and array, like Array.filter(), but only keeps x if pred(x) returns false.
 
 // Filters an array's values based on a predicate function, returning only values for which the predicate function returns false.
